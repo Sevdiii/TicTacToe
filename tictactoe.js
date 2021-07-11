149be2074e49320fbff6cb92d0ab6572cab95b35
Sevdi Azizi <you@example.com>
2021-07-11 23:23:47 +0200

    first commit

--- tictactoe.js
+++ tictactoe.js
@@ -51,45 +51,53 @@ function CheckForWin() {
     //First Row
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0]
-        document.getElementById('line-0').style.transform = 'scaleX(1)'
+        document.getElementById('line-0').style.transform = 'scaleX(1)';
+
 
     }
     //second Row
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3]
-        document.getElementById('line-1').style.transform = 'scaleX(1)'
+        document.getElementById('line-1').style.transform = 'scaleX(1)';
+
     }
 
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6]
-        document.getElementById('line-2').style.transform = 'scaleX(1)'
+        document.getElementById('line-2').style.transform = 'scaleX(1)';
+
     }
     // Schiefe Row
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0]
-        document.getElementById('line-6').style.transform = ' rotate(45deg) scaleX(1)'
-    }
+        document.getElementById('line-6').style.transform = ' rotate(45deg) scaleX(1)';
 
+    }
+    // schiefe Row
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2]
-        document.getElementById('line-7').style.transform = ' rotate(-45deg) scaleX(1)'
+        document.getElementById('line-7').style.transform = ' rotate(-45deg) scaleX(1)';
+
     }
 
     // first colum
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0]
 
-        document.getElementById('line-3').style.transform = ' rotate(90deg) scaleX(1)'
-    }
+        document.getElementById('line-3').style.transform = 'scaleY(1)';
 
+    }
+    // second colum
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1]
-        document.getElementById('line-4').style.transform = ' rotate(90deg)  scaleX(1)'
-    }
+        document.getElementById('line-4').style.transform = 'scaleY(1)';
 
+    }
+    // third colum
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2]
-        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)'
+        document.getElementById('line-5').style.transform = 'scaleY(1)';
+
     }
     if (winner) {
         console.log('Gewonnen', winner);
@@ -110,13 +118,16 @@ function restart() {
     document.getElementById('gameover').classList.add('d-none');
     document.getElementById('restart-btn').classList.add('d-none');
 
+
     for (let i = 0; i < 8; i++) {
-        document.getElementById('line-' + i).classList.add('d-none');
+        document.getElementById('line-' + i).style.transform = 'scale(0)'
+
     }
 
     for (let i = 0; i < 9; i++) {
         document.getElementById('circle' + i).classList.add('d-none');
         document.getElementById('x' + i).classList.add('d-none');
+
     }
 
 
